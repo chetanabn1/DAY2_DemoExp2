a2e5ef61ea80ac47a1f58375feba5374cdc82c4c
chetanabn1 <bnchetana@gmail.com>
2026-03-12 12:30:35 +0530

    ready for deploy

---
+++ src/test/java/org/test/WebPageTest.java
@@ -0,0 +1,39 @@
+package org.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterTest;
+
+public class WebPageTest {
+
+    private static WebDriver driver;
+
+    @BeforeTest
+    public void openBrowser() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.get("https://chetanabn1.github.io/DAY2_DemoExp2/");
+    }
+
+    @Test
+    public void titleValidationTest() {
+        String actualTitle = driver.getTitle();
+        System.out.println("Page Title: " + actualTitle);
+
+        String expectedTitle = "simple website";
+
+        Assert.assertEquals(actualTitle, expectedTitle, "Page title doesn't match!");
+        Assert.assertTrue(actualTitle.contains("website"));
+    }
+
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
+
